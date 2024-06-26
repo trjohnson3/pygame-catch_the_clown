500c7f1774597cc04fd78269af1bca3f0da8805d
Tom Johnson <trjohnson3@gmail.com>
2024-06-26 09:05:59 -0500

    more setup

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, random
 
 #Initialize game
 pygame.init()
@@ -8,6 +8,47 @@ WINDOW_WIDTH = 945
 WINDOW_HEIGHT = 600
 display = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Catch the Clown')
+
+#Set FPS and clock
+FPS = 60
+clock = pygame.time.Clock()
+
+#Set game values
+PLAYER_STARTING_LIVES = 5
+CLOWN_STARTING_VELOCITY = 3
+CLOWN_ACCELERATION = .5
+
+score = 0
+player_lives = PLAYER_STARTING_LIVES
+
+clown_velocity = CLOWN_STARTING_VELOCITY
+clown_dx = random.choice([-1, 1])
+clown_dy = random.choice([-1, 1])
+
+#Set colors
+BLUE = (1, 175, 209)
+YELLOW = (248, 231, 28)
+
+#Set fonts
+font = pygame.font.Font('./fonts/Franxurter.ttf', 32)
+
+#Set text
+title_text = font.render('Catch the Clown', True, Blue)
+title_rect = title_text.get_rect()
+title_rect.topleft(50, 10)
+
+score_text = font.render('Lives: ' + str(score), True, YELLOW)
+score_rect = score_text.get_rect()
+score_rect.topright(WINDOW_WIDTH - 50, 10)
+
+player_lives_text = font.render('Lives: ' + str(player_lives), True, YELLOW)
+player_lives_rect = player_lives_text.get_rect()
+player_lives_rect.topright(WINDOW_WIDTH - 50, 50)
+
+#Set sounds
+
+
+#Set images
 
 
 #Main game loop
